11d9a703b4984fd74b8aa39c4e25b2922b635b99
DenVik <denyagarkusha2@gmail.com>
2024-05-10 23:53:59 +0200

    scroll

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // модальне вікно
-const closeButton=document.querySelector('.button-modal-close');
-const isButton=document.querySelector('.order__button');
-const openModal=document.querySelector('.is-open');
-const closeModal=document.querySelector('.modal-overlay');
+const closeModalButton=document.querySelector('.button-modal-close');
+const openModalButton=document.querySelector('.order__button');
+const modal=document.querySelector('.modal-overlay');
+const scrollUp=document.querySelector('.is-open-scroll');
+
+// open modal
+openModalButton.addEventListener('click', ()=>{
+	modal.classList.toggle('is-open');
+	});
+// close modal
+closeModalButton.addEventListener('click', ()=>{
+	modal.classList.remove('is-open');
+	});
+// open modal + remove scroll
+	openModalButton.addEventListener('click', ()=>{
+	scrollUp.classList.remove('is-open-scroll');
+});
+// close modal + toggle scroll
+closeModalButton.addEventListener('click', ()=>{
+	scrollUp.classList.toggle('is-open-scroll');
+	});
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // бургер меню
-const closeButtonMedia=document.querySelector('.close-media-button');
+const closeMediaButton=document.querySelector('.close-media-button');
 const burgerButton=document.querySelector('.burger-menu');
-const openMenu=document.querySelector('.is-open-menu');
 const closeMenu=document.querySelector('.media-menu');
 
-const itemMenu1=document.querySelector('.media-list-button1');
-const itemMenu2=document.querySelector('.media-list-button2');
-const itemMenu3=document.querySelector('.media-list-button3');
+const buttonItemMenu1=document.querySelector('.media-list-button1');
+const buttonItemMenu2=document.querySelector('.media-list-button2');
+const buttonItemMenu3=document.querySelector('.media-list-button3');
 
-// модальне вікно
-isButton.addEventListener('click', ()=>{
-	closeModal.classList.toggle('is-open');
-	});
-
-
-closeButton.addEventListener('click', ()=>{
-	closeModal.classList.remove('is-open');
-	});
-
-
-// бургер меню
+// open menu
 burgerButton.addEventListener('click', ()=>{
 	closeMenu.classList.toggle('is-open-menu');
 	});
-closeButtonMedia.addEventListener('click', ()=>{
+// close menu
+closeMediaButton.addEventListener('click', ()=>{
+	closeMenu.classList.remove('is-open-menu');
+});
+// close menu
+buttonItemMenu1.addEventListener('click', ()=>{
+	closeMenu.classList.remove('is-open-menu');
+});
+// close menu
+buttonItemMenu2.addEventListener('click', ()=>{
+	closeMenu.classList.remove('is-open-menu');
+});
+// close menu
+buttonItemMenu3.addEventListener('click', ()=>{
 	closeMenu.classList.remove('is-open-menu');
 });
 
 
-itemMenu1.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
-});
-itemMenu2.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
-});
-itemMenu3.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
-});
+// ~~~~~~~~3 open bottom ~~~~~~~~~~~~~~~~
+const scrollOpen = document.querySelector('.scroll-up.is-open-scroll');
 
-// в JS можу тільки щось відкривати і закривати, на данний момент, логіу цьго процесу розумію))
+window.addEventListener('scroll', function(){
+	scrollOpen.classList.toggle("active", window.scrollY>500)
+})
