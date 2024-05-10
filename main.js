a742f40d7ce6cccc8024890b48630da561bb5046
DenVik <denyagarkusha2@gmail.com>
2024-05-11 00:30:11 +0200

    ddd

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~1
 // модальне вікно
-const closeModalButton=document.querySelector('.button-modal-close');
-const openModalButton=document.querySelector('.order__button');
 const modal=document.querySelector('.modal-overlay');
-const scrollUp=document.querySelector('.is-open-scroll');
+
+const openModalButton=document.querySelector('.order__button');
+
+const closeModalButton=document.querySelector('.button-modal-close');
 
 // open modal
 openModalButton.addEventListener('click', ()=>{
@@ -14,51 +15,44 @@ openModalButton.addEventListener('click', ()=>{
 closeModalButton.addEventListener('click', ()=>{
 	modal.classList.remove('is-open');
 	});
-// open modal + remove scroll
-	openModalButton.addEventListener('click', ()=>{
-	scrollUp.classList.remove('is-open-scroll');
-});
-// close modal + toggle scroll
-closeModalButton.addEventListener('click', ()=>{
-	scrollUp.classList.toggle('is-open-scroll');
-	});
 
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2
 // бургер меню
-const closeMediaButton=document.querySelector('.close-media-button');
-const burgerButton=document.querySelector('.burger-menu');
-const closeMenu=document.querySelector('.media-menu');
+const mediaMenu=document.querySelector('.media-menu');
 
+const burgerButton=document.querySelector('.burger-menu');
+
+const closeMediaButton=document.querySelector('.close-media-button');
 const buttonItemMenu1=document.querySelector('.media-list-button1');
 const buttonItemMenu2=document.querySelector('.media-list-button2');
 const buttonItemMenu3=document.querySelector('.media-list-button3');
 
-// open menu
+//1 open menu
 burgerButton.addEventListener('click', ()=>{
-	closeMenu.classList.toggle('is-open-menu');
+	mediaMenu.classList.toggle('is-open-menu');
 	});
-// close menu
+//2 close menu
 closeMediaButton.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
+	mediaMenu.classList.remove('is-open-menu');
 });
-// close menu
+//3 close menu
 buttonItemMenu1.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
+	mediaMenu.classList.remove('is-open-menu');
 });
-// close menu
+//4 close menu
 buttonItemMenu2.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
+	mediaMenu.classList.remove('is-open-menu');
 });
-// close menu
+//5 close menu
 buttonItemMenu3.addEventListener('click', ()=>{
-	closeMenu.classList.remove('is-open-menu');
+	mediaMenu.classList.remove('is-open-menu');
 });
 
 
-// ~~~~~~~~3 open bottom ~~~~~~~~~~~~~~~~
-const scrollOpen = document.querySelector('.scroll-up.is-open-scroll');
+// ~~~~~~~~3 open bottom ~~~~~~~~~~~~~~~~3
+const scrollUp=document.querySelector('.scroll-up');
 
 window.addEventListener('scroll', function(){
-	scrollOpen.classList.toggle("active", window.scrollY>500)
+	scrollUp.classList.toggle("is-open-scroll", window.scrollY>500)
 })
